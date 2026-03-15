25f416cd231811feaad203adaf8967b4950778d9
Rody Huancas <rodyhuancas.04@gmail.com>
2026-03-15 14:27:40 -0500

    Feat: create ForgotPasswordPage component with dynamic metadata, heading, and navigation links for login and account creation

---
+++ src/app/auth/forgot-password/page.tsx
@@ -0,0 +1,31 @@
+import { Metadata } from "next";
+import Heading from "@/shared/components/typography/Heading";
+import ForgotPasswordForm from "@/features/components/ForgotPasswordForm";
+import { generatePageTitle } from "@/shared/utils/metadata";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: generatePageTitle("Reestablecer Contraseña"),
+};
+
+const ForgotPasswordPage = () => {
+  return (
+    <>
+      <Heading>Recuperar tu acceso a Meeti</Heading>
+
+      <ForgotPasswordForm />
+
+      <nav className="mt-20 flex justify-between items-center">
+        <Link href="/auth/login" className="font-bold">
+          Iniciar Sesión
+        </Link>
+
+        <Link href="/auth/create-account" className="font-bold">
+          Crear Cuenta
+        </Link>
+      </nav>
+    </>
+  );
+};
+
+export default ForgotPasswordPage;
