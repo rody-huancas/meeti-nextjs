5355b6c9bbdae7fa7304d56294d0922c6d906c58
Rody Huancas <rodyhuancas.04@gmail.com>
2026-03-15 14:27:25 -0500

    Feat: enhance CreateAccountPage with Heading and RegisterForm components, and add navigation links for login and password recovery

--- src/app/auth/create-account/page.tsx
+++ src/app/auth/create-account/page.tsx
@@ -1,14 +1,31 @@
+import Link from "next/link";
 import { Metadata } from "next";
+import Heading from "@/shared/components/typography/Heading";
+import RegisterForm from "@/features/components/RegisterForm";
 import { generatePageTitle } from "@/shared/utils/metadata";
 
 export const metadata: Metadata = {
-  title: generatePageTitle("Crear Cuenta")
-}
+  title: generatePageTitle("Crear Cuenta"),
+};
 
 const CreateAccountPage = () => {
   return (
-    <div>CreateAccountPage</div>
-  )
-}
+    <>
+      <Heading>Crear Cuenta</Heading>
 
-export default CreateAccountPage
+      <RegisterForm />
+
+      <nav className="mt-20 flex justify-between items-center">
+        <Link href="/auth/login" className="font-bold">
+          Iniciar Sesión
+        </Link>
+
+        <Link href="/auth/forgot-password" className="font-bold">
+          Olvidé mi contraseña
+        </Link>
+      </nav>
+    </>
+  );
+};
+
+export default CreateAccountPage;
