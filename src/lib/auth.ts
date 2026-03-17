65cba0017eb4be65ca2abe2a292cd5df10a11069
Rody Huancas <rodyhuancas.04@gmail.com>
2026-03-16 21:22:19 -0500

    Feat: implement user authentication flow with session management and verification processes in drizzle-orm

---
+++ src/lib/auth.ts
@@ -0,0 +1,12 @@
+import { betterAuth } from "better-auth";
+import { nextCookies } from "better-auth/next-js";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "@/database";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    usePlural: true,
+  }),
+  plugins: [nextCookies()],
+});
