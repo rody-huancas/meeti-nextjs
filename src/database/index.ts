0919dbc695e3dbd2cc0f5ac89e75c7c643096c2c
Rody Huancas <rodyhuancas.04@gmail.com>
2026-03-16 21:22:01 -0500

    Feat: enhance database connection setup by integrating relations in drizzle-orm configuration

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,6 @@
 import { drizzle } from "drizzle-orm/node-postgres";
+import { relations } from "./relations";
 
-export const db = drizzle(process.env.DATABASE_URL!);
+export const db = drizzle(process.env.DATABASE_URL!, {
+  relations,
+});
