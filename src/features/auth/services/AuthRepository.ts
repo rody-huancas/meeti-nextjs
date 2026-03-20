5a75647a04f03bd86fe27e458e284c65622fc530
Rody Huancas <rodyhuancas.04@gmail.com>
2026-03-19 21:49:51 -0500

    Feat: implement user existence check in AuthRepository with database query

--- src/features/auth/services/AuthRepository.ts
+++ src/features/auth/services/AuthRepository.ts
@@ -1,3 +1,16 @@
-class AuthRepository {}
+import { db } from "@/database";
+import { User } from "../types/auth.types";
+
+export interface IAuthRepository {
+  userExists(email: string): Promise<User | undefined>;
+}
+
+class AuthRepository implements IAuthRepository {
+  async userExists(email: string) {
+    return await db.query.users.findFirst({
+      where: { email },
+    });
+  }
+}
 
 export const authRepository = new AuthRepository();
