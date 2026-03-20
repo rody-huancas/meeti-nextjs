fcc9db028e78285580a4480592536b06a6d17ef2
Rody Huancas <rodyhuancas.04@gmail.com>
2026-03-19 21:49:56 -0500

    Feat: implement user registration logic with existence check and success/error responses in AuthService

--- src/features/auth/services/AuthService.ts
+++ src/features/auth/services/AuthService.ts
@@ -1,15 +1,33 @@
+import { auth } from "@/lib/auth";
 import { SignUpInput } from "../schemas/auth.schema";
+import { authRepository, IAuthRepository } from "./AuthRepository";
 
 class AuthService {
+  constructor(private authRepository: IAuthRepository) {}
+
   async register(credentials: SignUpInput) {
     const { name, email, password } = credentials;
 
     // Revisar si el usuario existe
+    const user = await this.authRepository.userExists(email);
 
-    // Validación de negocio
+    if (user) {
+      return {
+        error: "Este E-mail ya está registrado",
+        success: "",
+      };
+    }
 
     // Manejar registro
+    await auth.api.signUpEmail({
+      body: { name, email, password },
+    });
+
+    return {
+      error: "",
+      success: "Cuenta creada correctamente, revisa tu E-mail.",
+    };
   }
 }
 
-export const authService = new AuthService();
+export const authService = new AuthService(authRepository);
